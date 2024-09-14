73f9559dbd6306ebf3730c0f29b26c365fa20901
joynul24 <joynula919@gmail.com>
2024-09-14 13:06:47 +0600

    added seat div push

--- app.js
+++ app.js
@@ -32,6 +32,23 @@ for (const btn of allBtns) {
         };
         setInnerText('seat-count', seatCoutnUpdate);
         // set the seat price div
+        const seatContainer = document.getElementById('selected-seat-container');
+        
+        const div = document.createElement('div');
+        div.classList.add('setInnerText-div');
+
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+        const p3 = document.createElement('p');
+
+        p1.innerText = event.target.innerText;
+        p2.innerText = 'Economoy';
+        p3.innerText = '550';
+
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+        seatContainer.appendChild(div);
 
     });
 };
